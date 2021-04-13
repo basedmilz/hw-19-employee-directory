1c9c9883357e7d3246dd4faab68298f6f6c6eba7
basedmilz <basedmilz@gmail.com>
2021-04-13 10:00:52 -0400

    Updates

--- src/App.js
+++
@@ -1,9 +0,0 @@
-import logo from './logo.svg';
-import './App.css';
-import Table from './components/Table';
-
-function App() {
-return <Table/>
-}
-
-export default App;
--- src/utils/api.js
+++
@@ -1,6 +0,0 @@
-import axios from "axios";
-const api = {
-    fetchEmployees: () => axios.get('https://randomuser.me/api/?results=50')
-
-}
-export default api;
